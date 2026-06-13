8a2d1cc667b36d0265e103df4bc61ee9a906034d
Lucas <lucassivolella@gmail.com>
2026-06-13 14:59:58 -0300

    Adiciona o script da entrega final

--- sql/estrega4.sql
+++ sql/estrega4.sql
@@ -1,27 +1,13 @@
--- TIPO ETAPA
+-- (1) DDL (DROP, ALTER, )
+-- Comandos DROP
 DROP TABLE IF EXISTS tipo_etapa CASCADE;
+DROP TABLE IF EXISTS etapa_do_processo CASCADE;
 
+-- Comandos CREATE
 CREATE TABLE IF NOT EXISTS tipo_etapa (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(150) NOT NULL
 );
-
-COMMENT ON TABLE tipo_etapa
-    IS 'Tabela responsável por armazenar o nome do tipo da etapa do processo.';
-
-COMMENT ON COLUMN tipo_etapa.id IS 'Identificador único do tipo de etapa.';
-COMMENT ON COLUMN tipo_etapa.nome IS 'Nome ou classificação do tipo de etapa (ex: Entrevista RH, Teste Técnico, Dinâmica de Grupo).';
-
-INSERT INTO tipo_etapa (nome) VALUES
-    ('Análise Curricular'),
-    ('Entrevista RH'),
-    ('Entrevista Técnica'),
-    ('Prova Lógica'),
-    ('Desafio Técnico'),
-    ('Entrevista Gestores');
-
--- ETAPA DO PROCESSO
-DROP TABLE IF EXISTS etapa_do_processo CASCADE;
 
 CREATE TABLE IF NOT EXISTS etapa_do_processo (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -35,18 +21,7 @@ CREATE TABLE IF NOT EXISTS etapa_do_processo (
     id_vaga INT NOT NULL
 );
 
-COMMENT ON TABLE etapa_do_processo
-    IS 'Tabela responsável por armazenar o histórico e o detalhamento das etapas de um determinado processo.';
-
-COMMENT ON COLUMN etapa_do_processo.id IS 'Identificador único do registro da etapa do processo.';
-COMMENT ON COLUMN etapa_do_processo.nome IS 'Nome específico dado a esta etapa dentro do processo.';
-COMMENT ON COLUMN etapa_do_processo.descricao IS 'Campo opcional para inserção de informações extras sobre a etapa.';
-COMMENT ON COLUMN etapa_do_processo.ordem_etapa IS 'Ordem em que a etapa ocorre dentro do processo seletivo.';
-COMMENT ON COLUMN etapa_do_processo.data_etapa IS 'Data agendada para o acontecimento da etapa.';
-COMMENT ON COLUMN etapa_do_processo.id_func_responsavel IS 'Chave estrangeira que referencia o funcionário responsável por conduzir ou avaliar esta etapa.';
-COMMENT ON COLUMN etapa_do_processo.id_tipo_etapa IS 'Chave estrangeira que referencia a categoria ou tipo desta etapa.';
-COMMENT ON COLUMN etapa_do_processo.id_vaga IS 'Chave estrangeira que referencia a vaga de emprego à qual esta etapa de processo pertence.';
-
+-- Comandos ALTER (criação de constraints)
 ALTER TABLE etapa_do_processo
     ADD CONSTRAINT fk_etapas_func
     FOREIGN KEY (id_func_responsavel) REFERENCES funcionario(id);
@@ -58,6 +33,34 @@ ALTER TABLE etapa_do_processo
 ALTER TABLE etapa_do_processo
     ADD CONSTRAINT fk_etapas_vaga
     FOREIGN KEY (id_VAGA) REFERENCES vaga(id);
+
+-- Comandos COMMENT
+-- tipo_etapa
+COMMENT ON TABLE tipo_etapa
+    IS 'Tabela responsável por armazenar o nome do tipo da etapa do processo.';
+COMMENT ON COLUMN tipo_etapa.id IS 'Identificador único do tipo de etapa.';
+COMMENT ON COLUMN tipo_etapa.nome IS 'Nome ou classificação do tipo de etapa (ex: Entrevista RH, Teste Técnico, Dinâmica de Grupo).';
+
+-- etapa_do_processo
+COMMENT ON TABLE etapa_do_processo
+    IS 'Tabela responsável por armazenar o histórico e o detalhamento das etapas de um determinado processo.';
+COMMENT ON COLUMN etapa_do_processo.id IS 'Identificador único do registro da etapa do processo.';
+COMMENT ON COLUMN etapa_do_processo.nome IS 'Nome específico dado a esta etapa dentro do processo.';
+COMMENT ON COLUMN etapa_do_processo.descricao IS 'Campo opcional para inserção de informações extras sobre a etapa.';
+COMMENT ON COLUMN etapa_do_processo.ordem_etapa IS 'Ordem em que a etapa ocorre dentro do processo seletivo.';
+COMMENT ON COLUMN etapa_do_processo.data_etapa IS 'Data agendada para o acontecimento da etapa.';
+COMMENT ON COLUMN etapa_do_processo.id_func_responsavel IS 'Chave estrangeira que referencia o funcionário responsável por conduzir ou avaliar esta etapa.';
+COMMENT ON COLUMN etapa_do_processo.id_tipo_etapa IS 'Chave estrangeira que referencia a categoria ou tipo desta etapa.';
+COMMENT ON COLUMN etapa_do_processo.id_vaga IS 'Chave estrangeira que referencia a vaga de emprego à qual esta etapa de processo pertence.';
+
+-- (2) DML
+INSERT INTO tipo_etapa (nome) VALUES
+    ('Análise Curricular'),
+    ('Entrevista RH'),
+    ('Entrevista Técnica'),
+    ('Prova Lógica'),
+    ('Desafio Técnico'),
+    ('Entrevista Gestores');
 
 INSERT INTO etapa_do_processo (nome, descricao, ordem_etapa, data_etapa, id_func_responsavel, id_tipo_etapa, id_vaga) VALUES
     ('Triagem Inicial', 'Leitura e filtro inicial de currículos recebidos', 1, '2026-03-01', 2, 1, 1),
@@ -75,3 +78,15 @@ INSERT INTO etapa_do_processo (nome, descricao, ordem_etapa, data_etapa, id_func
     ( 'Painel Técnico', 'Entrevista com 3 especialistas da equipe', 3, '2026-05-12', 9, 3, 8),
     ( 'Entrevista Fit Cultural', 'Bate-papo focado em valores da empresa', 1, '2026-06-01', 1, 2, 10),
     ( 'Bate-papo com CEO', 'Reunião de fechamento para cargos de liderança', 2, '2026-06-10', 10, 6, 10);
+
+-- (3) Consultas
+-- Q1
+-- Q2
+-- Q3
+-- Q4
+-- Q5
+-- Q6
+-- Q7
+-- Q8
+-- Q9
+-- Q10
